f888eacab1bbaee721b8b946df1670c6781e30e4
My Name <myemail@example.com>
2023-04-17 20:38:30 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+ import { useState, useEffect } from 'react';
+ import Axios from 'axios';
+ function App() {
+const [createdActivity, setGeneratedActivity] = useState('');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const fetchActivity =  ()=>{
+    Axios.get('http://www.boredapi.com/api/activity/').then(
+      (res) => { setGeneratedActivity(res.data.activity);}
+    )
+
+};
+return(
+    <div className='App'>
+      <h2> Create an activity for the week end</h2>
+      <button onClick={()=>fetchActivity()}>Create activity</button>
+      <p>{createdActivity}</p>
     </div>
-  );
-}
-
+  )
+  };
+  
 export default App;
+
+
+
+// import "./App.css";
+// import Axios from "axios";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [generatedExcuse, setGeneratedExcuse] = useState("");
+
+//   const fetchExcuse = (excuse) => {
+//     Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+//       (res) => {
+//         setGeneratedExcuse(res.data[0].excuse);
+//       }
+//     );
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1> Generate An Excuse </h1>
+//       <button onClick={() => fetchExcuse("party")}> Party</button>
+//       <button onClick={() => fetchExcuse("family")}> Family</button>
+//       <button onClick={() => fetchExcuse("office")}> Office </button>
+
+//       <p> {generatedExcuse} </p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
